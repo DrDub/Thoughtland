802fb7dc68802b550eee43e7e136d09a06b86ab2
Pablo Duboue <pablo.duboue@gmail.com>
2013-02-12 00:27:39 -0500

    * started integrating the schemata * added by-attribute schema

--- src/main/scala/net/duboue/thoughtland/nlg/simplenlg/generator.scala
+++ src/main/scala/net/duboue/thoughtland/nlg/simplenlg/generator.scala
@@ -29,10 +29,69 @@ import net.duboue.thoughtland.Generator
 import net.duboue.thoughtland.Paragraph
 import net.duboue.thoughtland.Sentence
 import net.duboue.thoughtland.RelativeMagnitude
+import net.sf.openschema.RDFOntology
+import net.sf.openschema.Ontology
+import net.sf.openschema.OpenSchemaPlanner
+import net.sf.openschema.SimpleFocusChooser
+import net.sf.openschema.util.SchemaToXmlFilterStream
+import net.sf.openschema.GreedyChooser
+import org.xml.sax.InputSource
+import net.sf.openschema.FrameSet
+import java.io.InputStream
+import net.sf.openschema.DocumentPlan
+import scala.collection.JavaConversions._
+import net.sf.openschema.Frame
 
 class SimpleNlgGenerator extends Generator {
 
-  def apply(analysis: Analysis)(implicit env: Environment): GeneratedText = null;
+  val ontology = new RDFOntology(classOf[SimpleNlgGenerator].getResourceAsStream("ontology.rdfs"),
+    "classpath://net/duboue/thoughtland/nlg/simplenlg/ontology.rdfs");
 
+  val byComponentSchema = new OpenSchemaPlanner(new InputSource(new SchemaToXmlFilterStream(classOf[SimpleNlgGenerator].getResourceAsStream("by-component.schema"))), new SimpleFocusChooser(ontology));
+  val byAttributeSchema = new OpenSchemaPlanner(new InputSource(new SchemaToXmlFilterStream(classOf[SimpleNlgGenerator].getResourceAsStream("by-attribute.schema"))), new GreedyChooser());
+
+  def apply(analysis: Analysis)(implicit env: Environment): GeneratedText = {
+    val frames = analysisToFrameSet(analysis);
+    val texts = List(byComponentSchema, byAttributeSchema)
+      .map { _.instantiate(frames, new java.util.HashMap(), ontology) }
+      .map(verbalize);
+    if (texts(0).toString.length < texts(1).toString.length)
+      return texts(0)
+    else
+      return texts(1)
+  }
+
+  protected def analysisToFrameSet(analysis: Analysis): FrameSet = new FrameSet() {
+    //TODO
+    def getFrame(x$1: String): net.sf.openschema.Frame = null
+    def getFrames(): java.util.Collection[net.sf.openschema.Frame] = null
+  }
+
+  protected def verbalize(plan: DocumentPlan): GeneratedText =
+    new GeneratedText(plan.getParagraphs().map {
+      aggrSegments =>
+        var clauses: List[java.util.Map[String, Object]] = List();
+        for (aggr <- aggrSegments)
+          clauses = clauses ++ aggr;
+        Paragraph(clauses.filter { !_.containsKey("template") }.map {
+          clause =>
+            val template = clause.get("template").toString();
+            val instantiated = new StringBuffer();
+            val fields = template.split("\\@");
+            instantiated.append((if (fields(0).startsWith("\"")) fields(0).substring(1) else fields(0)));
+            for (i <- 1.to(fields.length)) {
+              val nameRest = fields(i).split("\\.", 2);
+              if (clause.containsKey(nameRest(0)) && clause.get(nameRest(0)) != null) {
+                val value = clause.get(nameRest(0)).toString();
+                instantiated.append(if (value.startsWith("\"")) value.substring(1, value.length() - 1) else value);
+              }
+              instantiated.append(if (i == fields.length - 1 && nameRest(1).endsWith("\"")) nameRest(1).substring(0,
+                nameRest(1).length() - 1)
+              else nameRest(1));
+            }
+
+            Sentence(instantiated.toString())
+        })
+    }.toList)
 }
 
